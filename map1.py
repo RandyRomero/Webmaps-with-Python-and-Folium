cde66d5704b1b41c6986c2123b6d60995059c3b3
RandyRomero <a.ya.miheev@gmail.com>
2018-01-02 22:18:17 +0300

    Add marker pointing to Las Vegas High Roller, add comments, create .gitignore to exclude pycharm working files from tracking

--- map1.py
+++ map1.py
@@ -1,4 +1,17 @@
-import folium
+import folium  # module to convert python code to html/css/javascript
 
+# Create map object with world map centered in Las Vegas
 our_map = folium.Map(location=[36.174642, -115.091515], zoom_start=4)
+
+# Create layer that can group several items which you can turn on and off simultaneously later on
+fg = folium.FeatureGroup(name='My Map')
+
+# Add marker pointing to Las Vegas High Roller
+fg.add_child(folium.Marker(location=[36.117941, -115.168246], popup='High Roller',
+                           icon=folium.Icon(color='green')))
+
+our_map.add_child(fg)
+
+# Create actual interactive map
 our_map.save('Map1.html')
+
